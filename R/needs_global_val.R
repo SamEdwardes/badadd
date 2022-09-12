93ddc290a5f4fd7464aa96893cc5082bd5416f00
SamEdwardes <edwardes.s@gmail.com>
2022-09-12 21:41:28 +0000

    another update 0.0.2

--- R/needs_global_val.R
+++ R/needs_global_val.R
@@ -1,10 +1,12 @@
 #' Needs a global variable
 #'
-#' @return
+#' @return A string with "Nothing"
 #' @export
 #'
 #' @examples
+#' print("hello")
 needs_global_var <- function() {
   print("The message is:")
   print(my_message)
+  "Nothing"
 }
